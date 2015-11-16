463e4dbae215932c1202290dadbcdd4f7c7b1cd3
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2015-11-16 19:03:04 +0000

    compiles it all

--- main/Main.cpp
+++ main/Main.cpp
@@ -10,13 +10,14 @@ using namespace std;
 
 int main ()
 {
-    /*
     map<string,double> fiducial_params;
     vector<string> params;
-
-    Model_Santos2006 model(fiducial_params);
-    Tomography2D analysis(&model);
+    int Pk_index = 0;
+    int Tb_index = 0;
+    int q_index = 0;
+    Model_CAMB_ARES model(fiducial_params, &Pk_index, &Tb_index, &q_index);
+    Cosmology3D analysis(&model);
     Fisher1 fisher(&analysis, "test_output.dat", params);
-    */
+    
     return 0;
 }
