5a80e0a59c65742f093bddaa0307de084649df40
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2016-05-31 13:19:20 +0100

    update

--- plot_bispectrum_tetrapyd.py
+++ plot_bispectrum_tetrapyd.py
@@ -1,13 +1,17 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
-fig, ax = plt.subplots()
-xs1 = []
+
+fig =  plt.figure()
+ax = fig.gca(projection='3d')
 ys1 = []
 
 filename1 = "LISW_tetrapyd.dat"
 
 f1 = file(filename1)
 data = np.loadtxt(f1)
+
 
