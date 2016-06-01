078dc2be7cbb39f00e9b20e4d79c7e0b043b0bbe
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2016-06-01 19:31:41 +0100

    triangle plotting changes

--- plot_triangle_bispectrum.py
+++ plot_triangle_bispectrum.py
@@ -2,13 +2,25 @@ import numpy as np
 import copy
 import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 fig, ax = plt.subplots()
+plt.xlabel(r'$l_2/l_1$',fontsize=16)
+plt.ylabel(r'$l_3/l_1$', fontsize=16)
+ax.annotate(r'folded', xy = (0.5,0.5), xytext=(0.7,0.6), arrowprops=dict(arrowstyle='->',facecolor='black'))
+ax.annotate(r'squeezed', xy = (0.0,1.0), xytext=(0.05,0.75), arrowprops=dict(arrowstyle='->',facecolor='black'))
+ax.annotate(r'equilateral', xy = (1.0,1.0), xytext=(0.9,0.75), arrowprops=dict(arrowstyle='->',facecolor='black'))
+
+ax.annotate('', xy = (0.01,0.98), xytext=(0.48, 0.51), arrowprops=dict(arrowstyle='<->',facecolor='black'))
+ax.annotate('', xy = (0.99,0.98), xytext=(0.52, 0.51), arrowprops=dict(arrowstyle='<->',facecolor='black'))
+ax.annotate('elongated', xy = (0.22,0.72),xytext=(0.22, 0.72), rotation=-45)
+ax.annotate('isoceles', xy = (0.22,0.72),xytext=(0.72, 0.71), rotation=45)
+
 xs1 = []
 ys1 = []
 
-filename1 = "LISW_triangle_xy.dat"
-filename2 = "LISW_triangle_values.dat"
+filename1 = "output/Bispectrum/Triangle_plots/LISW_triangle_xy.dat"
+filename2 = "output/Bispectrum/Triangle_plots/LISW_triangle_values.dat"
 
 f1 = file(filename1)
 f2 = file(filename2)
@@ -36,13 +48,19 @@ data_reduced = copy.copy(data_new)
 for i in range(0,rows-1):
     for j in range(0,columns-1):
         if data_new[i][j] == 0:
-            data_reduced[i][j] = 0
+            data_reduced[i][j] = np.nan
         else:
             data_reduced[i][j] = math.log(data_new[i][j]/max_val)
 #print data_reduced
 middle = 0.5;
 extent = (0,1,0.5,1)
 plt.imshow(data_new,extent=extent,interpolation='none')
-plt.colorbar()
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(cax=cax)
+plt.title('Small difference between \n $B_{l_1,l_2,l_3}$ and $B_{max}$', y = 1.03, fontsize=16)
+plt.xlabel('\n \n Large difference between \n $B_{l_1,l_2,l_3}$ and $B_{max}$')
+#ax.annotate('Large difference between \n $B_{l_1,l_2,l_3}$ and $B_{max}$', xy = (0.22,0.72),xytext=(0.94, 0.44), fontsize=16)
+plt.ylabel(r'$\ln\left(\frac{B_{l_1,l_2,l_3}}{B_{max}}\right)$', fontsize=16)
 
 plt.show()
